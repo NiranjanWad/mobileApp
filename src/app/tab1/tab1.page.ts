3fc35344a6d1de51bb18b5cab79f5be8bc5fb028
Niranjan Wad <rider@Niranjans-MacBook-Pro.local>
2021-03-02 21:43:08 -0500

    back to mobile apps

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { PhotoService} from '../services/photo.service'
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private photoService: PhotoService) {}
+
+  async ngOnInit(){
+    await this.photoService.loadSaved();
+  }
+
+  addPhotoToGallery(){
+    this.photoService.addNewTogallery();
+  }
 
 }
